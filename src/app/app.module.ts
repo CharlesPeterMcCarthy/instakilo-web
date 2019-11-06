7331bb56a3342cbc5fc8a684699ddcf49776c2f5
James Coggins <S00171151@mail.itsligo.ie>
2019-11-06 14:04:59 +0000

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,24 @@ import { NavLinkComponent } from './components/nav/nav-link/nav-link.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AmplifyAngularModule, AmplifyModules, AmplifyService } from 'aws-amplify-angular';
 import Auth from '@aws-amplify/auth';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    NavLinkComponent
+    NavLinkComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
-    AmplifyAngularModule
+    AmplifyAngularModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
