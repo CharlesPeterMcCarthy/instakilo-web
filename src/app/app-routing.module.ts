b2d5c059886ecd753d2e11db9ce1bc988ee2138b
Charles McCarthy <charles@studybundles.com>
2019-11-17 23:08:01 +0000

    Route Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { LoginComponent } from './pages/login/login.component'
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { ConfirmEmailComponent } from './pages/confirm-email/confirm-email.component';
+import { LoggedInGuard } from './guards/logged-in/logged-in.guard';
+import { LoggedOutGuard } from './guards/logged-out/logged-out.guard';
 
 const routes: Routes = [
-  { path: 'create-post', component: CreatePostComponent },
-  { path: 'signup', component: SignUpComponent },
-  { path: 'confirm/:username/:code', component: ConfirmEmailComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'create-post', component: CreatePostComponent, canActivate: [ LoggedInGuard ] },
+  { path: 'signup', component: SignUpComponent, canActivate: [ LoggedOutGuard ] },
+  { path: 'confirm/:username/:code', component: ConfirmEmailComponent, canActivate: [ LoggedOutGuard ] },
+  { path: 'login', component: LoginComponent, canActivate: [ LoggedOutGuard ] }
 ];
 
 @NgModule({
