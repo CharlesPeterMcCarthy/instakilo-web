ae5a8e244b67736efcbf562bdba42a6af9debed3
Charles McCarthy <charles@studybundles.com>
2020-01-10 20:02:57 +0000

    Search Results Pipe

---
+++ src/app/pipes/generic-search-results/generic-search-results.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GenericSearchResultsPipe } from './generic-search-results.pipe';
+
+describe('GenericSearchResultsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GenericSearchResultsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
